c06791efe5b8cfada38dfd3cda9ad705871b0b00
Ruanpao <ltcdz114514@gmail.com>
2025-12-07 20:51:31 +0800

    Created more HUD

---
+++ Source/declassify_door/Public/UI/InventoryMainWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InventoryMainWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DECLASSIFY_DOOR_API UInventoryMainWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};

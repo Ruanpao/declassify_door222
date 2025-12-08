e933127d4d7efb5684fe83766d3608de7d5ac87b
Ruanpao <ltcdz114514@gmail.com>
2025-12-08 15:38:34 +0800

    InventoryComponent test

--- Source/declassify_door/Public/UI/HeldItemWidget.h
+++ Source/declassify_door/Public/UI/HeldItemWidget.h
@@ -4,6 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "declassify_door/CoreTypes/ItemCoreType.h"
+#include "Components/TextBlock.h"
+#include "Components/HorizontalBox.h"
 #include "HeldItemWidget.generated.h"
 
 /**
@@ -13,5 +16,44 @@ UCLASS()
 class DECLASSIFY_DOOR_API UHeldItemWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "DataTable")
+	UDataTable* DataTable;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "ItemInfo")
+	FName ID = "0000";
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "ItemInfo")
+	int32 Quantity = 0;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "ItemInfo")
+	FText Name = FText::FromString("None");
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "ItemInfo")
+	FText ItemType = FText::FromString("Invalid ItemType");
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite , Category = "ItemInfo")
+	UTexture2D* Icon;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite , Category = "ItemInfo")
+	FText CurrentBulletNum = FText::FromString("0");
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite , Category = "ItemInfo")
+	FText MaxBulletNum = FText::FromString("0");
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "CellUI" , meta = (BindWidget))
+	UTextBlock* NameText;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "CellUI" , meta = (BindWidget))
+	UHorizontalBox* BulletNumBox;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "CellUI" , meta = (BindWidget))
+	UHorizontalBox* ItemQuantityBox;
+
+	void UpdateHeldItemWidget(FItemInInventory HeldItem);
+
+protected:
+	virtual void NativeOnInitialized() override;
 	
 };

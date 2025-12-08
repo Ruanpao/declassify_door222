a26198b3e4027948203035d618832bb554b90dbe
Ruanpao <ltcdz114514@gmail.com>
2025-12-08 12:12:20 +0800

    More UI

--- Source/declassify_door/Public/UI/DisposalPopWidget.h
+++ Source/declassify_door/Public/UI/DisposalPopWidget.h
@@ -4,14 +4,46 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/Button.h"
+#include "Components/VerticalBox.h"
+#include "Components/CanvasPanel.h"
 #include "DisposalPopWidget.generated.h"
 
 /**
  * 
  */
+DECLARE_MULTICAST_DELEGATE_OneParam(FRemove_2 , bool)
+
 UCLASS()
 class DECLASSIFY_DOOR_API UDisposalPopWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+	FRemove_2 Remove_2;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite , Category = "UI" ,  meta = (BindWidget))
+	UButton* Button_0;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite , Category = "UI" ,  meta = (BindWidget))
+	UButton* Button_1;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite , Category = "UI" ,  meta = (BindWidget))
+	UCanvasPanel* CanvasPanel;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite , Category = "UI" ,  meta = (BindWidget))
+	UVerticalBox* VerticalBox;
+
+	UFUNCTION(BlueprintCallable)
+	void OnButton_0Clicked();
+
+	UFUNCTION(BlueprintCallable)
+	void OnButton_1Clicked();
+
+	UFUNCTION(BlueprintCallable)
+	void OnMouseLeave_1();
+	
+protected:
+	virtual void NativeOnInitialized() override;
 	
 };
--- Source/declassify_door/Public/UI/InteractPopWidget.h
+++ Source/declassify_door/Public/UI/InteractPopWidget.h
@@ -4,14 +4,49 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/Button.h"
+#include "Components/VerticalBox.h"
+#include "Components/CanvasPanel.h"
+#include "Blueprint/UserWidget.h"
+#include "declassify_door/declassify_doorCharacter.h" 
+#include "declassify_door/CoreTypes/ItemCoreType.h"
 #include "InteractPopWidget.generated.h"
 
 /**
  * 
  */
+
+
+DECLARE_MULTICAST_DELEGATE_FiveParams(FWhetherClickedAndNearTower, bool , bool , bool , bool , bool);
+
+DECLARE_MULTICAST_DELEGATE_OneParam(FSendHeldItem, FItemInInventory)
+
 UCLASS()
 class DECLASSIFY_DOOR_API UInteractPopWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
 	
+public:
+	FSendHeldItem SendHeldItem;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "DataTable")
+	UDataTable* DataTable;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "HeldItem")
+	FItemInInventory HeldItem;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite , Category = "UI" ,  meta = (BindWidget))
+	UButton* Button_TakeInHand;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite , Category = "UI" ,  meta = (BindWidget))
+	UCanvasPanel* CanvasPanel;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite , Category = "UI" ,  meta = (BindWidget))
+	UVerticalBox* VerticalBox;
+
+protected:
+	virtual void NativeOnInitialized() override;
+	void UpdateHeldItem(FItemInInventory NewHeldItem);
+
 };

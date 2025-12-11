e0aeac42fb12eaa88b3f8714629a0296a1a058dd
Ruanpao <ltcdz114514@gmail.com>
2025-12-11 22:32:47 +0800

    Final door teleport

--- Source/declassify_door/Private/Actor/FinalDoor.cpp
+++ Source/declassify_door/Private/Actor/FinalDoor.cpp
@@ -1,57 +1,83 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Actor/FinalDoor.h"
-
 #include "Actor/DoorPiece.h"
 #include "Kismet/GameplayStatics.h"
-
+#include "MainMenu/MainMenuPlayerController.h"
 
 AFinalDoor::AFinalDoor()
 {
-	PrimaryActorTick.bCanEverTick = true;
+    PrimaryActorTick.bCanEverTick = true;
 
-	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
-	DoorMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DoorMesh"));
-	DoorMesh->SetupAttachment(RootComponent);
+    RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+    DoorMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DoorMesh"));
+    DoorMesh->SetupAttachment(RootComponent);
 
-	DoorMesh->SetVisibility(false);
+    DoorMesh->SetVisibility(false);
+    
+    DoorMesh->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+    DoorMesh->SetCollisionResponseToAllChannels(ECR_Ignore);
+    DoorMesh->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
 }
 
 void AFinalDoor::BeginPlay()
 {
-	Super::BeginPlay();
+    Super::BeginPlay();
 
-	TArray<AActor*> FoundPieces;
-	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ADoorPiece::StaticClass(), FoundPieces);
+    TArray<AActor*> FoundPieces;
+    UGameplayStatics::GetAllActorsOfClass(GetWorld(), ADoorPiece::StaticClass(), FoundPieces);
 
-	for(AActor* Piece : FoundPieces)
-	{
-		ADoorPiece* PieceActor = Cast<ADoorPiece>(Piece);
-		if(PieceActor)
-		{
-			PieceActor->OnDoorOpened.AddDynamic(this,&AFinalDoor::AddKey);
-		}
-	}
+    for(AActor* Piece : FoundPieces)
+    {
+        ADoorPiece* PieceActor = Cast<ADoorPiece>(Piece);
+        if(PieceActor)
+        {
+            PieceActor->OnDoorOpened.AddDynamic(this, &AFinalDoor::AddKey);
+        }
+    }
 }
 
 void AFinalDoor::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
-
+    Super::Tick(DeltaTime);
 }
 
 void AFinalDoor::AddKey()
 {
-	CurrentNumberOfKeys++;
-	
-	if(CurrentNumberOfKeys == 3)
-	{
-		DoorMesh->SetVisibility(true);
-		if(DoorCreateSound)
-		{
-			UGameplayStatics::PlaySoundAtLocation(this,DoorCreateSound,GetActorLocation());
-		}
-	}
+    CurrentNumberOfKeys++;
+    
+    if(CurrentNumberOfKeys == 3)
+    {
+        DoorMesh->SetVisibility(true);
+        // 启用碰撞，允许交互
+        DoorMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+        
+        if(DoorCreateSound)
+        {
+            UGameplayStatics::PlaySoundAtLocation(this, DoorCreateSound, GetActorLocation());
+        }
+    }
+}
+
+void AFinalDoor::OnInteract_Implementation(AActor* Interactor)
+{
+    // 只有在门可见时才允许交互
+    if (DoorMesh->IsVisible())
+    {
+        UE_LOG(LogTemp, Warning, TEXT("FinalDoor interacted - Returning to main menu"));
+        
+        // 回到主菜单
+        ReturnToMainMenu();
+    }
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("FinalDoor is not visible - Interaction ignored"));
+    }
+}
+
+void AFinalDoor::ReturnToMainMenu()
+{
+        // 备用方案：直接加载主菜单地图
+    UGameplayStatics::OpenLevel(GetWorld(), "MenuMap");
 }
 
--- Source/declassify_door/Public/Actor/FinalDoor.h
+++ Source/declassify_door/Public/Actor/FinalDoor.h
@@ -4,31 +4,43 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Interface/InteractInterface.h" // 添加接口头文件
 #include "FinalDoor.generated.h"
 
 UCLASS()
-class DECLASSIFY_DOOR_API AFinalDoor : public AActor
+class DECLASSIFY_DOOR_API AFinalDoor : public AActor, public IInteractInterface
 {
 	GENERATED_BODY()
-	
-public:	
-	
+    
+public: 
 	AFinalDoor();
 
 protected:
 	virtual void BeginPlay() override;
 
-public:	
+public: 
 	virtual void Tick(float DeltaTime) override;
 
 	UFUNCTION()
 	void AddKey();
+    
+	// 实现交互接口
+	UFUNCTION(BlueprintCallable, Category = "Interaction")
+	virtual void OnInteract_Implementation(AActor* Interactor) override;
+
+    
 private:
-	UPROPERTY(EditAnywhere,Category = "FinalDoor")
+	UFUNCTION()
+	void ReturnToMainMenu();
+    
+	UPROPERTY(EditAnywhere, Category = "FinalDoor")
 	class UStaticMeshComponent* DoorMesh;
 
 	int32 CurrentNumberOfKeys = 0;
 
 	UPROPERTY(EditAnywhere, Category = "FinalDoor")
 	class USoundBase* DoorCreateSound;
+    
+	UPROPERTY(EditAnywhere, Category = "FinalDoor")
+	class USoundBase* InteractSound; // 交互音效
 };
